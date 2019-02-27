ac1341ddc7e121456f8fe0505cbe9e9f8462df40
Evandah-Steadman <vandahs@outlook.com>
2019-02-28 11:52:35 +1300

    nav component finished

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Nav () {
   return (
     <div>
-      <h2>Nav</h2>
+      {/* logo div */}
+      <div>
+        <h1>Track-a-grad</h1>
+      </div>
+      {/* login div */}
+      <div>
+        <h1>Login</h1>
+      </div>
     </div>
   )
 }
