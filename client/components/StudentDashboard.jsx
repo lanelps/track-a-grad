5a4ccd14623535e49155c6e54eb1a770d15a0afb
Lane Wirihana le Prevost-Smith <lanelps@gmail.com>
2019-02-28 11:26:08 +1300

    Added nav component

--- client/components/StudentDashboard.jsx
+++ client/components/StudentDashboard.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 
+import Nav from './Nav'
+
 export default class StudentDashboard extends Component {
   render () {
     return (
       <React.Fragment>
         <h1>Student Dashboard</h1>
+        <Nav />
       </React.Fragment>
     )
   }
