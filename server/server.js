05bddc3d6e243e6c11f296c3327f24b54e97f98e
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-28 11:34:20 +1300

    basic route and function added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const path = require('path')
 const express = require('express')
 const server = express()
 
+const graduateRoutes = require('./routes/graduate')
+const authRoutes = require('./routes/auth')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/api/v1/graduate', graduateRoutes)
+server.use('/api/v1/auth', authRoutes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 module.exports = server
