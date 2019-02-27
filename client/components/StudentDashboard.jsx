f5443653d2917662100fbbf8011c0e0bbf46c519
Lane Wirihana le Prevost-Smith <lanelps@gmail.com>
2019-02-28 11:21:10 +1300

    Created StudentDashboard component

---
+++ client/components/StudentDashboard.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class StudentDashboard extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Student Dashboard</h1>
+      </React.Fragment>
+    )
+  }
+}
