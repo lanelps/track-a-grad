f7aabc85142fde33549fb469de9dbb06841897d5
dluttig <dluttig5@live.com>
2019-02-28 17:29:50 +1300

    fixed speeling mistake

--- server/db/migrations/20190228170014_accomplishments.js
+++ server/db/migrations/20190228170014_accomplishments.js
@@ -1,6 +1,6 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('accomplishments', table => {
-    table.increment('id').primary()
+    table.increments('id').primary()
     table.integer('profile_id').references('profiles.id')
     table.string('description')
     table.string('year')
