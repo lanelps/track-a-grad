96365283fac3cec973af40c555fc07feb94a4b8b
Evandah-Steadman <vandahs@outlook.com>
2019-02-28 14:30:50 +1300

    finished client side routing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import SignIn from './SignIn'
 import StudentDashboard from './StudentDashboard'
+import StudentFeed from './StudentFeed'
 
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Route exact path ="/signIn" component={SignIn} />
-        <Route exact path="/List/:rank" component={List} />
-        <Route path="/List/:rank/:name" component={Description} />
-        <Route exact path="/List/:rank/:name/species" component={SpeciesListing} />
-        <Route exact path="/List/:rank/:name/species/:id" component={Species} />
-      </div>
+      <React.Fragment>
+        <Route exact path ="/" component={SignIn} />
+        <Route exact path="/studentDashboard" component={StudentDashboard} />
+        <Route exact path="/studentFeed" component={StudentFeed} />
+      </React.Fragment>
     </Router>
   )
 }
