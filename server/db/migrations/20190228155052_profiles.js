762fbe042d8602f82e69b01de10ca1b1e31281a1
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-02-28 17:51:06 +1300

    added seed and ammended migrations

--- server/db/migrations/20190228155052_profiles.js
+++ server/db/migrations/20190228155052_profiles.js
@@ -1,15 +1,14 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('profiles', table => {
     table.increments('id').primary()
-    table.integer('users_id').references('users.id')
-    table.integer('cohorts_id').references('cohorts.id')
-    table.string('profile_photo')
+    table.integer('user_id').references('users.id')
+    table.integer('cohort_id').references('cohorts.id')
+    table.string('profile_picture')
     table.string('location')
     table.string('cv_location')
     table.string('description')
     table.string('github_url')
-    table.string('string')
-    table.string('work_statuses').references('cohorts.users')
+    table.string('work_statuses_id')
   })
 }
 
---
+++ server/db/seeds/profiles.js
@@ -0,0 +1,20 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('profiles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('profiles').insert([
+        {
+          id: 1,
+          user_id: 1,
+          cohort_id: 1,
+          profile_picture: '',
+          location: '',
+          cv_location: '',
+          description: '',
+          github_url: '',
+          status: 'useless'
+        }
+      ])
+    })
+}
