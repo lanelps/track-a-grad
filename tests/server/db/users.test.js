2377b2e0f402191bcb5093d6ae5de3ad1ab3a202
RMH <riki@craftsmoothie.co.nz>
2019-02-28 15:31:39 +1300

    working on testing getGraduates

---
+++ tests/server/db/users.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest')
+
+const seedData = [
+  {
+    id: 1,
+    graduate_id: 1,
+    first_name: 'John',
+    last_name: 'Smith',
+    email: 'john.smith@mail.com',
+    hash: '',
+    boolean: 'true'
+  }
+]
+
+jest.mock('../../../server/db/users', () => ({
+  getGraduates: () => Promise.resolve({
+    id: 1,
+    graduate_id: 1,
+    first_name: 'John',
+    last_name: 'Smith',
+    email: 'john.smith@mail.com',
+    hash: '',
+    boolean: 'true'})
+}))
+
+const server = require('../../../server/server')
+
+test('GET users', () => {
+  return request(server)
+    .get('/api/v1/users')
+    .expect(200)
+    .then((res) => {
+      const expected = seedData[0].status_id
+      const actual = res.body.status_id
+      expect(expected).toBe(actual)
+    })
+    .catch(err => expect(err).toBeNull())
+})
