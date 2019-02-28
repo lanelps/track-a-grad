450eab5fede2ecdc8bc623802a3af578f1f726c6
Evandah-Steadman <vandahs@outlook.com>
2019-02-28 14:10:07 +1300

    adjusted client side routing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import SignIn from './SignIn'
 import StudentDashboard from './StudentDashboard'
+
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <StudentDashboard />
+    <Router>
+      <div>
+        <Route exact path ="/signIn" component={SignIn} />
+        <Route exact path="/List/:rank" component={List} />
+        <Route path="/List/:rank/:name" component={Description} />
+        <Route exact path="/List/:rank/:name/species" component={SpeciesListing} />
+        <Route exact path="/List/:rank/:name/species/:id" component={Species} />
+      </div>
+    </Router>
   )
 }
 
