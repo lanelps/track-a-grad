08cdaa73f6fd3c58803102376156f9be279e31ef
Evandah-Steadman <vandahs@outlook.com>
2019-03-01 16:28:49 +1300

    have added buttons to enable read only and write only

--- client/components/EditProfile.jsx
+++ client/components/EditProfile.jsx
@@ -5,12 +5,16 @@ export default class EditProfile extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      activeEdit: false
+      activeEdit: true,
+      name: ''
     }
     this.handleEdit = this.handleEdit.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
   handleEdit () {
     console.log('hit me')
     this.setState({
@@ -29,10 +33,13 @@ export default class EditProfile extends Component {
         <TextField
           id="standard-multiline-flexible"
           label="Multiline"
-          multiline = {!this.state.activeEdit}
-          disabled = {this.state.activeEdit}
-          rowsMax="4"
+          multiline = {this.state.activeEdit}
+          disabled = {!this.state.activeEdit}
+          rowsMax="5"
           margin="normal"
+          value={this.state.name}
+          name='name'
+          onChange={this.handleChange}
         />
         <button onClick={this.handleSubmit}> submit </button>
         <button onClick={this.handleEdit}> edit </button>
