c1ee9e1708ef3f9cae9179a9ddd3c511346eb89f
Jason James Simpson <jasonjamessimpson@gmail.com>
2019-03-01 14:39:42 +1300

    minor ammendment

---
+++ server/db/seeds/work_statuses.js
@@ -0,0 +1,12 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('work_statuses').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('work_statuses').insert([
+        {id: 1,
+          status: 'useless'
+        }
+      ])
+    })
+}
