7226934ca49fd618702115fa8bf5ee81a832b315
Evandah-Steadman <vandahs@outlook.com>
2019-03-02 14:34:43 +1300

    adjusted file structure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import SignIn from './SignIn'
+import SignIn from './SignIn/SignIn'
 import GraduateDashboard from './GraduateDashboard/GraduateDashboard'
-import GraduateFeed from './GraduateFeed'
-import Registration from './Registration'
+import GraduateFeed from './GraduateFeed/GraduateFeed'
+import Registration from './Registration/Registration'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import '../../server/public/main.css'
 
