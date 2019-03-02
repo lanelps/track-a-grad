259e9e6a08bbaae7d1dc15dd5d8c45b794ac345e
Evandah-Steadman <vandahs@outlook.com>
2019-03-02 20:57:26 +1300

    styled header info

--- client/components/GraduateProfile/GraduateProfile.jsx
+++ client/components/GraduateProfile/GraduateProfile.jsx
@@ -4,12 +4,21 @@ import './graduateprofile.css'
 export default function GraduateProfile (props) {
   return (
     <div className="graduateprofile">
-      {/* <img src={`${props.profilePicture}`} alt="student-profile-picture"/> */}
-      <img src="../../images/avatars/avatar32.png" width="100"/>
-      <h1>{props.firstName} {props.lastName}</h1>
-      <p>{props.status}</p>
-      <p>Status: {props.workStatus}</p>
-      <h3>{props.location}</h3>
+      <div className="profileHeader">
+        <div>
+          {/* <img src={`${props.profilePicture}`} alt="student-profile-picture"/> */}
+          <img src="../../images/avatars/avatar32.png" width="100"/>
+          <div className="info">
+            <h1>{props.firstName} {props.lastName}</h1>
+            <h4>{props.location}</h4>
+            <h4>Harakeke2019</h4> {/*  {props.cohort} */}
+          </div>
+        </div>
+        <div className="statusBar">
+          {/* <p>{props.status} </p> */}
+          <p>{props.workStatus}</p>
+        </div>
+      </div>
       <p>Point of difference: {props.description}</p>
     </div>
   )
