448f82b29a8a0a96e489952d94d70ed2cc14affc
Evandah-Steadman <vandahs@outlook.com>
2019-03-04 15:23:37 +1300

    setup redux for api call

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux'
 import graduates from './users'
 import profile from './profile'
-import userInformation from'./userInformation'
 import signIn from './auth'
 
 export default combineReducers({
   graduates,
   profile,
-  userInformation,
   signIn
 })
